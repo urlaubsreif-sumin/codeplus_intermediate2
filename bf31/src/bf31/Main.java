e94ac1e40149fd6b0001e03c62a03a4226fbefb7
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-03-11 11:45:53 +0900

    1806_부분합

---
+++ bf31/src/bf31/Main.java
@@ -0,0 +1,55 @@
+package bf31;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		try {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int S = Integer.parseInt(st.nextToken());
+			int[] A = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
+				A[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int L = 0;
+			int R = 0;
+			int sum = A[0];
+			int len = 1;
+			int min = 0;
+			while(L >= 0 && R < N) {
+				if(sum >= S) {
+					System.out.println(L + " " + R + " " + len);
+					if(min == 0 || min > len) {
+						min = len;
+					}
+				}
+				if(sum <= S) {
+					R++;
+					len++;
+					if(R < N)
+						sum += A[R];
+				}
+				else if(sum > S) {
+					sum -= A[L];
+					len--;
+					L++;
+				}
+			}
+			
+			System.out.println(min);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
