3967893d32f6dbc414f0b4acd44f9b4e076efa59
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-03-17 12:31:16 +0900

    1927_최소 힙

---
+++ heap2/src/heap2/Main.java
@@ -0,0 +1,39 @@
+package heap2;
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			int N = Integer.parseInt(br.readLine());
+			while(N --> 0) {
+				int x = Integer.parseInt(br.readLine());
+				if(x == 0) {
+					if(pq.isEmpty()) {
+						sb.append(0).append('\n');
+					}
+					else {
+						sb.append(pq.poll()).append('\n');
+					}
+				}
+				else {
+					pq.add(x);
+				}
+			}
+			
+			System.out.println(sb.toString());
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
