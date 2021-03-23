df80497886d719b088a32239d98153fb320fd093
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-03-23 17:04:11 +0900

    2281_데스노트

---
+++ dp21/src/dp21/Main.java
@@ -0,0 +1,54 @@
+package dp21;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] arr;
+	static long[][] d;
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			arr = new int[n];
+			d = new long[n][m + 2];
+			
+			for(int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			
+			for(int i = 0; i < n; i++) {
+				Arrays.fill(d[i], -1);
+			}
+			
+			long res = go(0, arr[0] + 1);
+			
+			System.out.println(res);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static long go(int i, int j) {
+		if(i == n - 1) {
+			return 0;
+		}
+		if(d[i][j] != -1)
+			return d[i][j];
+		long ans = go(i + 1, arr[i] + 1) + (m - j + 1) * (m - j + 1);
+		if(j + arr[i] <= m) {
+			long v2 = go(i + 1, j + arr[i] + 1);
+			if(ans > v2) {
+				ans = v2;
+			}
+		}
+		d[i][j] = ans;
+		return ans;
+	}
+
+}
